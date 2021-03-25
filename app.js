4b0651a232edba6c30565e0757698e9d5ca01e9f
BrianTheLion328 <78449517+BrianTheLion328@users.noreply.github.com>
2021-03-24 22:00:49 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,75 +1,106 @@
-let game;
-let totalTime = 30;
-let startTime;
-let endTime;
+let result = 0;
+let timeLeft = 30;
+let hardGameStart;
+let easyGameStart;
+let gameInPlay = false;
+// making moles appear on gameboard
 
 function buildMoles() {
-    $('.moles').removeClass('active-mole')
-    let moles = []
+  if (timeLeft >= 1) {
+    $(".moles").removeClass("active-mole");
+    // $(".moles").attr('id', '');
+    let moles = [];
     for (i = 0; i < 3; i++) {
-        let randomNumber = Math.floor(Math.random() * 10)
-        moles.push(randomNumber)
+      let randomNumber = Math.floor(Math.random() * 10);
+      moles.push(randomNumber);
     }
 
     moles.forEach(function (mole) {
-        $(`.mole${mole}`).addClass('active-mole')
-    })
-    buildRabbit()
+      $(`.mole${mole}`).addClass("active-mole");
+    //   $(`.mole${mole}`).attr('id', 'moleinplay')
+    });
 
-    // console.log(totalTime)
-    let now = new Date()
-    console.log('now: ' + now)
-    console.log('endtime: ' + endTime)
-    $('#timer-display').text(now - endTime)
-    if( now <= endTime){
-        clearInterval(game)
-    }
-
+    buildRabbit();
+  } else {
+    $(".moles").removeClass("active-mole");
+    $(".moles").removeClass("active-rabbit");
+    gameInPlay = false;
+  }
 }
+// making rabbit appear on gameboard
 
 function buildRabbit() {
-    $('.moles').removeClass('active-rabbit')
-    let moles = []
-    for (i = 0; i < 1; i++) {
-        let randomNumber = Math.floor(Math.random() * 10)
-
-        moles.push(randomNumber)
-    }
-
-    moles.forEach(function (mole) {
-        $(`.mole${mole}`).addClass('active-rabbit')
-    })
+  $(".moles").removeClass("active-rabbit");
+  let moles = [];
+  for (i = 0; i < 1; i++) {
+    let randomNumber = Math.floor(Math.random() * 10);
+    moles.push(randomNumber);
+  }
+  moles.forEach(function (mole) {
+    $(`.mole${mole}`).addClass("active-rabbit");
+  });
 }
 
 // start buttons section
 function easyGame() {
-    startTime = new Date()
-    endTime = new Date(startTime + 30000)
-   game = setInterval(buildMoles, 1000)
-}
+    // gameInPlay = true;
+  setInterval(buildMoles, 1000);
 
+}
 function hardGame() {
-    game = setInterval(buildMoles, 300)
+    // gameInPlay = true;
+  setInterval(buildMoles, 300);
 }
 
+const startButtonEasy = $(".starteasy");
+const startButtonHard = $(".starthard");
 
-$('button .starteasy').click(function (){
+startButtonEasy.click(function () {
+    timeLeft = 30;
+  $(this).attr("disabled", true);
+  easyGame();
+  countDown();
+//   if (!gameInPlay){
+//       clearInterval(easyGameStart)
+//       gameInPlay = false;
+//   }
+});
 
+startButtonHard.click(function () {
+    timeLeft = 30;
+  $(this).attr("disabled", true);
+  hardGame();
+  countDown();
+//   if (!gameInPlay){
+//     clearInterval(hardGameStart)
+//     gameInPlay = false;
+// }
+});
+
+// Countdown timer function
+
+const timeLeftDisplay = $("#timer-display");
+
+function countDown() {
+  setInterval(function () {
+    if (timeLeft <= 0) {
+      clearInterval((timeLeft = 0));
+      startButtonEasy.attr("disabled", false);
+      startButtonHard.attr("disabled", false);
+    }
+    timeLeftDisplay.html(timeLeft);
+    timeLeft -= 1;
+  }, 1000);
+}
+// startButtonEasy.click(countDown);
+// startButtonHard.click(countDown);
+
+$('#mole-grid').on('click', '.active-mole', function () {
+    result++
+    $('.score').text(result)
 })
 
-// easyGame()
-
-
-// $('.starteasy, starthard').click(function (){
-//     let timer = $('#timer-display').text()
-//     for (let i = 30; i >= 0; i--){
-
-//     }
-
-// })
-// const gameInterval = setInterval(startGame, 1000)
-// clearInterval(gameInterval)
-
-setInterval(function() {
-    $('.Timer').text((new Date - start) / 1000 + " Seconds");
-}, 1000);
+$('#mole-grid').on('click', '.active-rabbit', function () {
+    result--
+    $('.score').text(result)
+})
