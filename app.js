5a444acbf991974dbea20f755440f76e89468476
BrianTheLion328 <78449517+BrianTheLion328@users.noreply.github.com>
2021-03-25 15:53:38 -0700

    Add files via upload
    I created an function to add points to the score, a function to reset the clock/score/game when you click the start buttons, and made it simpler to click on moles/rabbit to add or subtract points after giving them a value of 1 and taking it away, then making it so only moles with a value of 1 can be clicked on, thanks to Elle's suggestion about adding a class or Id and making THAT clickable for points rather than the background picture of the moles or rabbit themselves which wasn't working.

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+let game;
+let totalTime = 30;
 let result = 0;
-let timeLeft = 30;
-let hardGameStart;
-let easyGameStart;
-let gameInPlay = false;
+
+const timeLeftDisplay = $("#timer-display");
+const startButtonEasy = $(".starteasy");
+const startButtonHard = $(".starthard");
+
 // making moles appear on gameboard
 
 function buildMoles() {
-  if (timeLeft >= 1) {
+
+//   Here, I'm setting the attribute "value" for all
+//   ".moles" DIVs to be reset to 1 after each cycle
+//   of setInterval in "easyGame()" and "hardGame()"
+  $('.moles').attr("value", 1)
+
+  if ($('#timer-display').text() >= 1) {
+
     $(".moles").removeClass("active-mole");
-    // $(".moles").attr('id', '');
     let moles = [];
     for (i = 0; i < 3; i++) {
       let randomNumber = Math.floor(Math.random() * 10);
@@ -17,16 +26,15 @@ function buildMoles() {
 
     moles.forEach(function (mole) {
       $(`.mole${mole}`).addClass("active-mole");
-    //   $(`.mole${mole}`).attr('id', 'moleinplay')
     });
 
     buildRabbit();
+
   } else {
-    $(".moles").removeClass("active-mole");
-    $(".moles").removeClass("active-rabbit");
-    gameInPlay = false;
+      $('.moles').removeClass("active-mole");
+      $('.moles').removeClass("active-rabbit");
   }
-}
+};
 // making rabbit appear on gameboard
 
 function buildRabbit() {
@@ -37,70 +45,155 @@ function buildRabbit() {
     moles.push(randomNumber);
   }
   moles.forEach(function (mole) {
+    $(`.mole${mole}`).removeClass("active-mole");
     $(`.mole${mole}`).addClass("active-rabbit");
   });
-}
+};
+
+// I added this function to simplify the
+// board clearing logic at the end of each game
+function clearBoard() {
+  $('.moles').removeClass("active-mole");
+  $('.moles').removeClass("active-rabbit");
+};
+
+// I added this function to simplify the
+// disable button logic at the beginning of each game
+function disableButtons() {
+  startButtonEasy.attr("disabled", true);
+  startButtonHard.attr("disabled", true);
+};
+
+// I added this function to simplify the
+// enable button logic at the end of each game
+function enableButtons() {
+  startButtonEasy.attr("disabled", false);
+  startButtonHard.attr("disabled", false);
+};
 
 // start buttons section
 function easyGame() {
-    // gameInPlay = true;
-  setInterval(buildMoles, 1000);
 
-}
+  // When a User begins a new Easy game,
+  // the countdown will be reset to 30
+  $('#timer-display').text(30);
+  $('.score').text(0);
+
+  // I refined this code to capture the return
+  // value of "setInterval" into the variable
+  // "timer," which is passed into clearInterval
+  let timer = setInterval(function () {
+
+    // I refined the logic here to make the
+    // "clearInterval()" and "clearBoard()"
+    // dependent upon the timer display being "0"
+    if ($('#timer-display').text() == 0) {
+      clearBoard();
+      clearInterval(timer);
+    } else {
+      buildMoles();
+    }
+  }, 1000);
+};
+
 function hardGame() {
-    // gameInPlay = true;
-  setInterval(buildMoles, 300);
-}
 
-const startButtonEasy = $(".starteasy");
-const startButtonHard = $(".starthard");
+  // When a User begins a new Hard game,
+  // the countdown will be reset to 30
+  $('#timer-display').text(30);
+  $('.score').text(0);
 
-startButtonEasy.click(function () {
-    timeLeft = 30;
-  $(this).attr("disabled", true);
-  easyGame();
+  // I refined the logic here to make the
+  // "clearInterval()" and "clearBoard()"
+  // dependent upon the timer display being "0"
+  let timer = setInterval(function () {
+    // I refined the logic here to make the
+    // "clearInterval()" and "clearBoard()"
+    // dependent upon the timer display being "0"
+    if ($('#timer-display').text() == 0) {
+      clearBoard();
+      clearInterval(timer);
+    } else {
+      buildMoles();
+    }
+  }, 400);
+};
+// changed the interval speed here to 400ms since 200ms was near impossible!
+startButtonEasy.click(function (event) {
+  // When a User clicks on "Start Game (Easy)",
+  // the "result" variable used in the "addPoints()"
+  // function and the score are automatically reset to "0"
+  result = 0;
+  disableButtons();
   countDown();
-//   if (!gameInPlay){
-//       clearInterval(easyGameStart)
-//       gameInPlay = false;
-//   }
+  // turn on clock and end game when clock reaches 0.
+  easyGame();
 });
 
 startButtonHard.click(function () {
-    timeLeft = 30;
-  $(this).attr("disabled", true);
-  hardGame();
+  // When a User clicks on "Start Game (Easy)",
+  // the "result" variable used in the "addPoints()"
+  // function and the score are automatically reset to "0"
+  result = 0;
+  disableButtons();
   countDown();
-//   if (!gameInPlay){
-//     clearInterval(hardGameStart)
-//     gameInPlay = false;
-// }
+  // turn on clock and end game when clock reaches 0.
+  hardGame();
 });
 
 // Countdown timer function
 
-const timeLeftDisplay = $("#timer-display");
-
 function countDown() {
-  setInterval(function () {
-    if (timeLeft <= 0) {
-      clearInterval((timeLeft = 0));
-      startButtonEasy.attr("disabled", false);
-      startButtonHard.attr("disabled", false);
-    }
-    timeLeftDisplay.html(timeLeft);
-    timeLeft -= 1;
+
+  // Call "addPoints" function to run at the beginning of each countdown
+  addPoints();
+
+  let timeLeft = 30;
+
+  // Here, I have captured the return value of "setInterval"
+  // into a variable named "timer" and used it to clear the
+  // interval
+  let timer = setInterval(function () {
+
+  if (timeLeft == 0) {
+    // Here, I'm using the return value of "setInterval"
+    // stored in "timer" to properly clear the interval.
+    clearInterval(timer);
+    enableButtons();
+  }
+
+  timeLeftDisplay.html(timeLeft);
+  timeLeft -= 1;
+
   }, 1000);
 }
-// startButtonEasy.click(countDown);
-// startButtonHard.click(countDown);
 
-$('#mole-grid').on('click', '.active-mole', function () {
-    result++
-    $('.score').text(result)
-})
+// adding points for hitting moles
+// here is where im having trouble below, not sure this is right?
 
-$('#mole-grid').on('click', '.active-rabbit', function () {
-    result--
-    $('.score').text(result)
-})
+const addPoints = function () {
+  // To begin, I set up the click behavior to be observable across all
+  // of the game board instead of just each mole circle.
+  $('.moles').click(function () {
+    // Here, the ".moles" DIV must have both the "active-mole" class and
+    // the "value" attribute as "1" in order to be clickable.
+    if ( $(this).hasClass("active-mole") && $(this).attr('value') != 0 ) {
+      // Here, I set the attribute "value" to "0" if it has been clicked.
+      // In other words, it's "used up" it's available clicks (1) for the
+      // current cycle.
+      $(this).attr('value', 0);
+      $(".score").text(result += 1);
+    } else if ( $(this).hasClass("active-rabbit") && $(this).attr('value') != 0 ) {
+      // Here, I set the attribute "value" to "0" if it has been clicked.
+      // In other words, it's "used up" it's available clicks (1) for the
+      // current cycle.
+      $(this).attr('value', 0);
+
+      // This prevents the user's score from going into negative values
+      // if they are already at "0"
+      if ( result != 0 ) {
+        $(".score").text(result -= 1);
+      }
+    }
+  });
+};
